9fdbe54f8c1e0ba7a4ca2c6a3dfd35d98b1d33d2
Danya D. Smith <danyadsmith@gmail.com>
2017-11-10 18:32:43 -0500

    08. Repeat a String - Tests and Solution
    This is an alternative solution to Repeat a String using a
    recursive pattern.

--- src/08_RepeatAString/index.js
+++ src/08_RepeatAString/index.js
@@ -2,11 +2,20 @@ function repeatStringNumTimes (str, num) {
   if (typeof str === 'string') {
     if (typeof num === 'number') {
       var repeated = '';
-      if (num < 0) { num = 0; }
-      for (let i = 0; i < num; i++) {
-        repeated += str;
+
+      let repeatString = function (str) {
+        return repeated += str;
+      };
+
+      if (num <= 0) { return repeated; }
+
+      while (num > 0) {
+        repeatString(str);
+        num--;
       }
+
       return repeated;
+
     } else {
       var errNumType = new TypeError('The parameter num must be a number.');
       throw errNumType;
